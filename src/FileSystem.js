439aca1c54ebf27634a715d5e9b3436633a50fdd
WHoefer <wolfgahoefer@gmail.com>
2017-11-15 21:38:25 +0100

    implement: getImagePath, getAudioPath, getVideoPath, getFlipbookPath, getSequencePath, getPagePath, getImageUri

--- src/FileSystem.js
+++ src/FileSystem.js
@@ -1,6 +1,71 @@
 import RNFetchBlob from 'react-native-fetch-blob';
 import {logger, loggerBookshelf} from './util/logging';
 
+export const BOOKSHELF = 0;
+export const IMAGE = 1;
+export const AUDIO = 2;
+export const VIDEO = 3;
+export const FLIPBOOK = 4;
+export const SEQUENCE = 5;
+export const PAGE = 6;
+export const PATH = 7;
+//export const
+
+export const getImagePath = (resource, key) => {
+    const imageDir = resource.content[BOOKSHELF].imagePath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[IMAGE][key];
+    return rootPath+imageDir+'/'+file;
+}
+
+export const getAudioPath = (resource, key) => {
+    const audioDir = resource.content[BOOKSHELF].audioPath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[AUDIO][key];
+    return rootPath+audioDir+'/'+file;
+}
+
+export const getVideoPath = (resource, key) => {
+    const videoDir = resource.content[BOOKSHELF].videoPath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[VIDEO][key];
+    return rootPath+videoDir+'/'+file;
+}
+
+export const getFlipbookPath = (resource, key) => {
+    const flipbookDir = resource.content[BOOKSHELF].flipbookPath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[FLIPBOOK][key];
+    return rootPath+flipbookDir+'/'+file;
+}
+
+export const getSequencePath = (resource, key) => {
+    const sequenceDir = resource.content[BOOKSHELF].sequencePath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[SEQUENCE][key];
+    return rootPath+sequenceDir+'/'+file;
+}
+
+export const getPagePath = (resource, key) => {
+    const pageDir = resource.content[BOOKSHELF].pagePath;
+    const rootPath = resource.content[PATH];
+    const file = resource.content[PAGE][key];
+    return rootPath+pageDir+'/'+file;
+}
+
+
+export const getImageUri = (resource, key) => {
+    return {'uri': `file://${getImagePath(resource, key)}`};
+}
+
+export const getAudioUri = (resource, key) => {
+    return {'uri': `file://${getAudioPath(resource, key)}`};
+}
+
+export const getVideoUri = (resource, key) => {
+    return {'uri': `file://${getVideoPath(resource, key)}`};
+}
+
 const getAssociativeArray = (log, obj, array, path) => {
   logger(2, log, 'getLocalJsonFile', path);
   const data = obj.content
@@ -13,85 +78,80 @@ const getAssociativeArray = (log, obj, array, path) => {
   return array;
 }
 
+const getRespObjResource = (path) => {
+  return {
+    statusCode: 0,
+    errorMessage: `Error: No content from ${path}/resources.json`
+  };
+}
+
+const getRespObjBookshelf = (path) => {
+  return {
+    statusCode: 0,
+    errorMessage: `Error: No content from ${path}/bookshelf.json`
+  };
+}
+
+
 export const getLocalRecourses = (log, path, responseHandler) => {
   let response = [];
+  response[PATH] = path;
   logger(1, log, 'getLocalRecourses from', path);
   getLocalJsonFile(path+'/bookshelf.json',  (obj) => {
      if(obj){
        logger(2, log, 'getLocalJsonFile', 'bookshelf.json');
        const bookShelf = obj;
-       response.push(bookShelf);
+       response[BOOKSHELF] = bookShelf;
        loggerBookshelf(3, log, bookShelf);
        getLocalJsonFile(path+bookShelf.imagePath+'/resources.json',  (objImage) => {
          if(objImage && objImage.type === 'IMAGE'){
-           response.push(getAssociativeArray(log, objImage, imgA = [], path+bookShelf.imagePath+'/resources.json'));
+           response[IMAGE] = getAssociativeArray(log, objImage, imgA = [], path+bookShelf.imagePath+'/resources.json');
            getLocalJsonFile(path+bookShelf.audioPath+'/resources.json',  (objAudio) => {
              if(objAudio && objAudio.type === 'AUDIO'){
-               response.push(getAssociativeArray(log, objAudio, audA = [], path+bookShelf.audioPath+'/resources.json'));
+               response[AUDIO] = getAssociativeArray(log, objAudio, audA = [], path+bookShelf.audioPath+'/resources.json');
                getLocalJsonFile(path+bookShelf.videoPath+'/resources.json',  (objVideo) => {
                  if(objVideo && objVideo.type === 'VIDEO'){
-                   response.push(getAssociativeArray(log, objVideo, vidA = [], path+bookShelf.videoPath+'/resources.json'));
+                   response[VIDEO] = getAssociativeArray(log, objVideo, vidA = [], path+bookShelf.videoPath+'/resources.json');
                    getLocalJsonFile(path+bookShelf.flipBookPath+'/resources.json',  (objFlipBook) => {
                      if(objFlipBook && objFlipBook.type === 'FLIPBOOK'){
-                       response.push(getAssociativeArray(log, objFlipBook, flbA = [], path+bookShelf.flipBookPath+'/resources.json'));
+                       response[FLIPBOOK] = getAssociativeArray(log, objFlipBook, flbA = [], path+bookShelf.flipBookPath+'/resources.json');
                        getLocalJsonFile(path+bookShelf.sequencePath+'/resources.json',  (objSequence) => {
                          if(objSequence && objSequence.type === 'SEQUENCE'){
-                           response.push(getAssociativeArray(log, objSequence, seqA = [], path+bookShelf.sequencePath+'/resources.json'));
+                           response[SEQUENCE] = getAssociativeArray(log, objSequence, seqA = [], path+bookShelf.sequencePath+'/resources.json');
                            getLocalJsonFile(path+bookShelf.pagePath+'/resources.json',  (objPage) => {
                              if(objPage && objPage.type === 'PAGE'){
-                               response.push(getAssociativeArray(log, objPage, pagA = [], path+bookShelf.pagePath+'/resources.json'));
+                               response[PAGE] = getAssociativeArray(log, objPage, pagA = [], path+bookShelf.pagePath+'/resources.json');
                                responseHandler({
                                  statusCode: 200,
                                  errorMessage: `No Errors`,
                                  content: response
                                });
                              }else{
-                               responseHandler({
-                                 statusCode: 0,
-                                 errorMessage: `Error: No content from ${path}${bookShelf.pagePath}/resources.json`
-                               });
+                               responseHandler(getRespObjResource(path + bookShelf.pagePath));
                              }
                            });// getLocalJsonFile(path+bookShelf.pagePath+'/resources.json',  (objPage) => {
                          }else{
-                           responseHandler({
-                             statusCode: 0,
-                             errorMessage: `Error: No content from ${path}${bookShelf.sequencePath}/resources.json`
-                           });
+                           responseHandler(getRespObjResource(path + bookShelf.sequencePath));
                          }
                        });// getLocalJsonFile(path+bookShelf.sequencePath+'/resources.json',  (objSequence) => {
                      }else{
-                       responseHandler({
-                         statusCode: 0,
-                         errorMessage: `Error: No content from ${path}${bookShelf.flipBookPath}/resources.json`
-                       });
+                       responseHandler(getRespObjResource(path + bookShelf.flipBookPath));
                      }
                    });// getLocalJsonFile(path+bookShelf.flipBookPath+'/resources.json',  (objFlipBook) => {
                  }else{
-                   responseHandler({
-                     statusCode: 0,
-                     errorMessage: `Error: No content from ${path}${bookShelf.videoPath}/resources.json`
-                   });
+                   responseHandler(getRespObjResource(path + bookShelf.videoPath));
                  }
                });// getLocalJsonFile(path+bookShelf.videoPath+'/resources.json',  (objVideo) => {
              }else{
-               responseHandler({
-                 statusCode: 0,
-                 errorMessage: `Error: No content from ${path}${bookShelf.imagePath}/resources.json`
-               });
+               responseHandler(getRespObjResource(path + bookShelf.audioPath));
              }
            });// getLocalJsonFile(path+bookShelf.audioPath+'/resources.json',  (objAudio) => {
          }else{
-           responseHandler({
-             statusCode: 0,
-             errorMessage: `Error: No content from ${path}${bookShelf.imagePath}/resources.json`
-           });
+           responseHandler(getRespObjResource(path + bookShelf.imagePath));
          }
        });// getLocalJsonFile(path+bookShelf.imagePath+'/resources.json',  (objImage) => {
      }else{
-       responseHandler({
-         statusCode: 0,
-         errorMessage: `Error: No content from ${path}/bookshelf.json`
-       });
+       responseHandler(getRespObjBookshelf(path));
      }
   }); // getLocalJsonFile(path+'/bookshelf.json',  (obj) => {
 }
